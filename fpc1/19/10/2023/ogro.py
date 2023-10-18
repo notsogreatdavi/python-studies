2dbde6189b68cf5d1874534950d047b276f82949
Davi Vieira <davivdsilva03@gmail.com>
2023-10-18 07:11:38 -0300

    loop do input e condição esquerda

--- fpc1/19/10/2023/ogro.py
+++ fpc1/19/10/2023/ogro.py
@@ -1,1 +1,9 @@
-input(int("Qual o número que você deseja contar? Lembre-se que ele deve ser maior ou igual a zero e menor que dez\n"))
+while True:
+    numero = int(input("Qual o número que você deseja contar? Lembre-se que ele deve ser maior ou igual a zero e menor ou igual a dez\n"))
+    if (numero > 0) or (numero < 10):
+        break
+
+    if numero <= 5:
+        for i in range (numero):
+            print("I")
+    
