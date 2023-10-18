2001878850efe5fb29b3ae44515a099a23bdf329
Davi Vieira <davivdsilva03@gmail.com>
2023-10-18 16:07:45 -0300

    fix range

--- fpc1/19/10/2023/zero.py
+++ fpc1/19/10/2023/zero.py
@@ -1,7 +1,9 @@
 numeros = []
 qnumeros = int(input("Quantos números o seu chefe falou, contando com os zeros?\n"))
 
-for i in qnumeros:
+for i in range(qnumeros):
     numero = input("Digite em ordem crescente os números ditos, linha por linha\n")
     if numero != 0:
         numeros.append(numero)
+    else:
+        del numeros[-1]
