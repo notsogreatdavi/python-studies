4b43b343c135b0b94d52d0319c92e9934565ac2b
Davi Vieira <davivdsilva03@gmail.com>
2023-10-18 15:27:24 -0300

    implementando restrições em falha.py

--- fpc1/19/10/2023/falha.py
+++ fpc1/19/10/2023/falha.py
@@ -1,11 +1,17 @@
 senhas = []
 contador = 0
-usuarios = int(input("Quantos usuários estão no sistema?\n"))
+while True: 
+    usuarios = int(input("Quantos usuários estão no sistema?\n"))
+    if usuarios >= 1 and usuarios <= 2000:
+        break
 for i in range(usuarios):
-    senhas.append(input("Digite a senha de um dos usuários restantes\n"))
+    while True:
+        senha = (input("Digite a senha de um dos usuários restantes\n"))
+        if len(senha) <= 10 and len(senha) >= 1 and senha.lower() == senha:
+            senhas.append(senha)
+            break
 
 for i in range(len(senhas)):
-
     for j in range (len(senhas)):
         if senhas[i] in senhas[j] and i != j:
             contador+= 1
---
+++ fpc1/19/10/2023/teste.py
@@ -0,0 +1,4 @@
+texto = 'meu texto tem caracteres com maiusculas'
+
+if all(texto.islower()):
+     print('tudo minusculo')
