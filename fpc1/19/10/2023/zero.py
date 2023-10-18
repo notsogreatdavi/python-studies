18abd831dcc495c0d4b74c835454729d8d3850c2
Davi Vieira <davivdsilva03@gmail.com>
2023-10-18 16:12:08 -0300

    done zero.py

--- fpc1/19/10/2023/zero.py
+++ fpc1/19/10/2023/zero.py
@@ -1,9 +1,14 @@
+somanumeros = 0
 numeros = []
 qnumeros = int(input("Quantos números o seu chefe falou, contando com os zeros?\n"))
 
 for i in range(qnumeros):
-    numero = input("Digite em ordem crescente os números ditos, linha por linha\n")
+    numero = int(input("Digite em ordem crescente os números ditos, linha por linha\n"))
     if numero != 0:
         numeros.append(numero)
     else:
         del numeros[-1]
+        
+for numero in numeros:
+    somanumeros += numero
+print(somanumeros)
