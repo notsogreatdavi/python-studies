f8ed939e80cd40021ff860a2d0609d296850c67c
Davi Vieira <davivdsilva03@gmail.com>
2023-10-18 16:24:39 -0300

    colocando restricoes zero.py

--- fpc1/19/10/2023/zero.py
+++ fpc1/19/10/2023/zero.py
@@ -1,14 +1,25 @@
 somanumeros = 0
 numeros = []
-qnumeros = int(input("Quantos números o seu chefe falou, contando com os zeros?\n"))
+while True:
+    qnumeros = int(input("Quantos números o seu chefe falou, contando com os zeros?\n"))
+    if qnumeros >= 1 and qnumeros <= 100000:
+        break
 
 for i in range(qnumeros):
-    numero = int(input("Digite em ordem crescente os números ditos, linha por linha\n"))
+    while True:
+        numero = int(input("Digite em ordem crescente os números ditos, linha por linha\n"))
+        if  numero >= 0 and numero <= 100:
+            break
+
     if numero != 0:
         numeros.append(numero)
     else:
         del numeros[-1]
-        
+
 for numero in numeros:
     somanumeros += numero
+
+if somanumeros > 1000000:
+    print("A soma passou do limite perdido")
+    
 print(somanumeros)
