f616350aee29b9915a6009d91f3f9ba13f76e884
Davi Vieira <davivdsilva03@gmail.com>
2023-10-18 15:50:06 -0300

    torneio.py criado e implementado

---
+++ fpc1/19/10/2023/torneio.py
@@ -0,0 +1,16 @@
+resultados = []
+vitorias_cont = 0
+
+for i in range(6):
+    resultados.append(input("Digite o resultado de seus jogos em ordem, um por vez em cada linha, sendo V vitória e P perda\n"))
+
+for i in range(len(resultados)):
+    if resultados[i] == 'v' or resultados[i] == 'V':
+        vitorias_cont += 1
+
+if vitorias_cont == 5 or vitorias_cont == 6:
+    print(1)
+if vitorias_cont == 3 or vitorias_cont == 4:
+    print(2)
+if vitorias_cont == 1 or vitorias_cont == 2:
+    print(3)
