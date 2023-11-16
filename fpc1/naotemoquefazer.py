0f80ba01f801a0dcf2944b8af0572eef31b40e08
Davi Vieira <davivdsilva03@gmail.com>
2023-11-15 21:39:34 -0300

    mudei funcao F de local

--- fpc1/naotemoquefazer.py
+++ fpc1/naotemoquefazer.py
@@ -1,13 +1,13 @@
-def G(n):
-   def F(n, contador):
-       if n == 1:
-           return contador + 1
-       elif n % 2 == 0:
-           return F(n // 2, contador + 1)
-       else:
-           return F(3 * n + 1, contador + 1)
-   return F(n, 0)
+def F(n, contador):
+    if n == 1:
+        return contador + 1
+    elif n % 2 == 0:
+        return F(n // 2, contador + 1)
+    else:
+        return F(3 * n + 1, contador + 1)
 
+def G(n):
+   return F(n, 0)
 
 qt_casos = int(input())
 for i in range(qt_casos):
