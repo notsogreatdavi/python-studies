e8b98ed6829bce62570ae57624f52ca56d125ce4
Davi Vieira <davivdsilva03@gmail.com>
2023-11-22 23:53:43 -0300

    brute force done

--- pisi2/flyfood.py
+++ pisi2/flyfood.py
@@ -3,14 +3,48 @@ listalinhas = arq.readlines()
 arq.close()
 linha_coluna = listalinhas[0].split()
 matriz = [""] * int(linha_coluna[0])
+paths = []
+pontos = {}
+distancias = {}
+caminhos_dist = {}
 
 for i in range(1, int(linha_coluna[1])):
     matriz[i-1] = listalinhas[i].split()
 
+for i, linha in enumerate(matriz):
+   for j, elemento in enumerate(linha):
+       if elemento != "0":
+           pontos[elemento] = (i, j)
 
-print(matriz)
+for chave in pontos:
+    for chave2 in pontos:
+        distancias[chave + chave2] = abs(pontos[chave][0] - pontos[chave2][0]) + abs(pontos[chave][1] - pontos[chave2][1])
+           
+def permutacoes(s):
+  if len(s) == 1:
+      return [s]
 
-# Calcular a distância de cada ponto para os outros pontos
-# Criar todas as possibilidade possíveis com os caminhos que foram dados e formar um caminho com cada um
-# Calcular a distância percorrida por cada um dos caminhos
-# Comparar os caminhos e printar o menor
+  permuta = []
+  for i in range(len(s)):
+      resto = s[:i] + s[i+1:]
+      for p in permutacoes(resto):
+          permuta.append(s[i:i+1] + p)
+  return permuta
+
+pontos_nomes = list(pontos.keys())
+str_pontos = ""
+for i in range(len(pontos_nomes)):
+    if str(pontos_nomes[i]) != "R":
+        str_pontos += str(pontos_nomes[i])
+caminhos = permutacoes(str_pontos)
+
+for elem in caminhos:
+    distancia_percorrida = 0
+    for i in range(len(elem) - 1):
+        if elem[i:i+2] in distancias:
+            distancia_percorrida += distancias[elem[i:i+2]]
+            caminhos_dist[elem] = distancia_percorrida
+
+caminhos_dist_ord = dict(sorted(caminhos_dist.items(), key=lambda item: item[1]))
+caminhos_ord = list(caminhos_dist_ord.keys())
+print(caminhos_ord[0])
